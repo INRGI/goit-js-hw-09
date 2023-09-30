da0f3a34288b7ef0f49c8243287dc8ed2372ffed
INRGI <120032162+INRGI@users.noreply.github.com>
2023-09-30 08:28:36 +0000

    Deploying to gh-pages from @ INRGI/goit-js-hw-09@77cae1d7f069351c3ae32960e3fbfd73afa611bb 🚀

---
+++ 01-color-switcher.df706467.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");function o(){return"#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}e.setAttribute("disabled","true"),t.addEventListener("click",(function(){document.body.style.backgroundColor=o(),setInterval((function(){document.body.style.backgroundColor=o()}),1e3),t.setAttribute("disabled","true"),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){}))}();
+//# sourceMappingURL=01-color-switcher.df706467.js.map
