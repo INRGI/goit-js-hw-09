77cae1d7f069351c3ae32960e3fbfd73afa611bb
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-09-30 10:28:01 +0200

    function onStart()

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+let timerId = null;
+
+stopBtn.setAttribute('disabled','true')
+
+function onStart() {
+    document.body.style.backgroundColor = getRandomHexColor();
+    timerId = setInterval(() => { document.body.style.backgroundColor = getRandomHexColor(); }, 1000);
+    startBtn.setAttribute('disabled', 'true');
+    stopBtn.removeAttribute('disabled')
+};
+
+function onStop() {
+    
+}
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
