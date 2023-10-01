c6ff3a8aab18289058bc718bd4d302f16ba67dda
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-10-01 18:59:40 +0200

    queryselectors and imports

---
+++ src/js/02-timer.js
@@ -0,0 +1,33 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const startBtn = document.querySelector('[data-start]');
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]]');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const flatpickr = require("flatpickr");
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
