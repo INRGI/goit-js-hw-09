98193725c2abece1b62eb36959c97eb3e8e67d4b
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-10-01 19:10:46 +0200

    css timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,13 +5,22 @@ const startBtn = document.querySelector('[data-start]');
 const days = document.querySelector('[data-days]');
 const hours = document.querySelector('[data-hours]');
 const minutes = document.querySelector('[data-minutes]');
-const seconds = document.querySelector('[data-seconds]]');
+const seconds = document.querySelector('[data-seconds]');
+let timerId = null;
 
+startBtn.setAttribute('disabled', true);
 
-
-
-
-
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
 
 
 
