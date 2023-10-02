494559bdc0eadfcca8773dd63bc133e597bbd7e8
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-10-02 20:06:20 +0200

    add function

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -40,15 +40,28 @@ flatpickr('#datetime-picker', { ...options });
 
 function onStart() {
   inputDate.setAttribute('disabled', 'true');
-  
+  timerId = setInterval(() => {
+    
+  },1000)
+}
+
+function stopTimer() {
+  if (
+    (days.textContent === '00') &
+    (hours.textContent === '00') &
+    (minutes.textContent === '00') &
+    (seconds.textContent === '00')
+  ) {
+    clearInterval(timerId);
+  }
 }
 
 function renderTime(time) {
   const convertTime = convertMs(time)
-  days.textContent = addZeroToStart(convertTime.days);
-  hours.textContent = addZeroToStart(convertTime.hours);
-  minutes.textContent = addZeroToStart(convertTime.minutes);
-  seconds.textContent = addZeroToStart(convertTime.seconds);
+  days.textContent = convertTime.days;
+  hours.textContent = convertTime.hours;
+  minutes.textContent = convertTime.minutes;
+  seconds.textContent = convertTime.seconds;
 }
 
 function convertMs(ms) {
@@ -56,10 +69,10 @@ function convertMs(ms) {
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const days = addZeroToStart(Math.floor(ms / day));
+  const hours = addZeroToStart(Math.floor((ms % day) / hour));
+  const minutes = addZeroToStart(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addZeroToStart(Math.floor((((ms % day) % hour) % minute) / second));
   return { days, hours, minutes, seconds };
 }
 
