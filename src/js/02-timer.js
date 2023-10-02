c69edadb7a802ee3c49617c581aa505a758fd02e
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-10-02 19:51:26 +0200

    add render to chosen date

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,14 +1,55 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+const inputDate = document.querySelector('[datetime-picker]');
 const startBtn = document.querySelector('[data-start]');
 const days = document.querySelector('[data-days]');
 const hours = document.querySelector('[data-hours]');
 const minutes = document.querySelector('[data-minutes]');
 const seconds = document.querySelector('[data-seconds]');
+
 let timerId = null;
 
 startBtn.setAttribute('disabled', true);
+
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const currentTime = new Date().getTime();
+    const selectedDate = selectedDates[0];
+      if (selectedDate < currentTime) {
+        window.alert("Please choose a date in the future")
+        return;
+      }
+    startBtn.removeAttribute('disabled');
+    renderTime(selectedDate - currentTime)
+      
+  },
+};
+
+startBtn.addEventListener('click', onStart);
+
+
+
+flatpickr('#datetime-picker', { ...options });
+
+function onStart() {
+  inputDate.setAttribute('disabled', 'true');
+  
+}
+
+function renderTime(time) {
+  const convertTime = convertMs(time)
+  days.textContent = addZeroToStart(convertTime.days);
+  hours.textContent = addZeroToStart(convertTime.hours);
+  minutes.textContent = addZeroToStart(convertTime.minutes);
+  seconds.textContent = addZeroToStart(convertTime.seconds);
+}
 
 function convertMs(ms) {
   const second = 1000;
@@ -22,21 +63,6 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-
-
-
-
-
-
-
-const flatpickr = require("flatpickr");
-
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
+function addZeroToStart(time) {
+  return time.toString().padStart(2, "0")
+}
