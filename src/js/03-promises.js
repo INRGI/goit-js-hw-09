2cd1640d870a644253ed51eeb84a163878466828
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-10-05 15:17:09 +0200

    add function createPromise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,16 +1,23 @@
 import Notiflix from 'notiflix';
 
 const formSelector = document.querySelector('.form');
+const delaySelector = document.querySelector('input[name="delay"]');
+const stepSelector = document.querySelector('input[name="step"]');
+const amountSelector = document.querySelector('input[name="amount"]');
 
+formSelector.addEventListener('submit', onSubmit)
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => { 
+      if (shouldResolve) {
+        resolve({ position, delay });
+    }
+      reject({ position, delay });
+    }, delay);
+  });
 }
 
 
-formSelector.addEventListener('submit', onSubmit)
+
