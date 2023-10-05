85138552305188232d56aa21e809e461b75f0690
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-10-05 15:43:15 +0200

    task 03-promises before

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,13 +7,41 @@ const amountSelector = document.querySelector('input[name="amount"]');
 
 formSelector.addEventListener('submit', onSubmit)
 
+function onSubmit(event) {
+  event.preventDefault();
+
+  let delay = Number(delaySelector.value);
+  const step = Number(stepSelector.value);
+  const amount = Number(amountSelector.value);
+  let position = 0;
+
+  if (delay <= 0 || step <= 0 || amount <= 0) {
+    return Notiflix.Notify.failure('Please enter a correct value');
+  }
+
+  for (let i = 1; i <= amount; i++){
+    position = i;
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
+  };
+  formSelector.reset();
+};
+
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-    setTimeout(() => { 
+
+    setTimeout(() => {
       if (shouldResolve) {
         resolve({ position, delay });
-    }
+      }
+
       reject({ position, delay });
     }, delay);
   });
